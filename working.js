3834cca006be2c6d358f600b42fc4803b7594f96
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-06-15 23:16:13 +0300

    incercare

--- working.js
+++ working.js
@@ -46,7 +46,29 @@ http.createServer(function (request, response) {
             response.end(content, 'utf-8');
         }
     });
-
+    onload()
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+/*
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}*/

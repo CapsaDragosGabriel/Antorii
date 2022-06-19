9f08f9b144223fd473fbd0d0c255fb6993cf6048
AndriciAndra <79244898+AndriciAndra@users.noreply.github.com>
2022-06-19 03:32:26 +0300

    d

--- ride-sharing/driver.js
+++ ride-sharing/driver.js
@@ -1,11 +1,10 @@
-function refreshRides()
-{
-    var x=document.getElementById("commandsList");
-    x.innerHTML="<h2 id=\"title\">Comenzi care te asteapta</h2>";
+function refreshRides() {
+    var x = document.getElementById("commandsList");
+    x.innerHTML = "<h2 id=\"title\">Comenzi care te asteapta</h2>";
 
 }
-async function getClaimedRides()
-{
+
+async function getClaimedRides() {
     const data = {
         token: localStorage.getItem('token')
     }
@@ -27,22 +26,22 @@ async function getClaimedRides()
 
             sent = false
         });
-    if (sent==true)
-    {
-        let length=globalRides.length;
-        if(response)
-        for (let i =0;i<response.length;i++)
-            globalRides[length+i]=response[i];
+    if (sent == true) {
+        let length = globalRides.length;
+        if (response)
+            for (let i = 0; i < response.length; i++)
+                globalRides[length + i] = response[i];
         console.log("RASPUNSUL A FOST" + response)
-        console.log("GLOBAL RIDES DUPA  GET CLAIMED RIDES" +globalRides);
+        console.log("GLOBAL RIDES DUPA  GET CLAIMED RIDES" + globalRides);
 
         // console.log(globalRides);
     }
 }
-async function updateRide(id,status){
+
+async function updateRide(id, status) {
     const data = {
-        id:id,
-        status:status,
+        id: id,
+        status: status,
         token: localStorage.getItem('token')
     }
     let sent = true;
@@ -65,24 +64,23 @@ async function updateRide(id,status){
         });
 
 
-
 }
-function showRides(){
-    var x=document.getElementById("commandsList");
+
+function showRides() {
+    var x = document.getElementById("commandsList");
     console.log(JSON.stringify(globalRides[0]));
     // console.log(newObj.start);
-    for (let i=0;i<globalRides.length;i++)
-{
-    if (globalRides[i])
-    {   var newObj=(globalRides[i]);
+    for (let i = 0; i < globalRides.length; i++) {
+        if (globalRides[i]) {
+            var newObj = (globalRides[i]);
 
-                var newCommand=document.createElement('div');
-                // console.log("PLECAM DE LA "+currRide);
-                newCommand.innerHTML=`<h1>${newObj.start} - ${newObj.finish}
+            var newCommand = document.createElement('div');
+            // console.log("PLECAM DE LA "+currRide);
+            newCommand.innerHTML = `<h1>${newObj.start} - ${newObj.finish}
             </h1>`;
-                newCommand.className="command";
-                newCommand.innerHTML=newCommand.innerHTML+
-                    `<label>
+            newCommand.className = "command";
+            newCommand.innerHTML = newCommand.innerHTML +
+                `<label>
                         <p>Status comanda: ${newObj.status}</p>
                        <!-- <select class="selectStatus" name="status">
                             <option value="none" selected disabled hidden>Status</option>
@@ -118,14 +116,16 @@ function showRides(){
                     }"
                     >Terminat</button>     
             `;
-                x.appendChild(newCommand);
-                var newRide=document.createElement('div')}
-    // console.log("una bucata ride"+currRide);
+            x.appendChild(newCommand);
+            var newRide = document.createElement('div')
+        }
+        // console.log("una bucata ride"+currRide);
+    }
 }
-}
+
 let globalRides;
-async function getNewRides()
-{
+
+async function getNewRides() {
     const data = {
         token: localStorage.getItem('token')
     }
@@ -147,9 +147,9 @@ async function getNewRides()
 
             sent = false
         });
- //   console.log("Raspunsul de la server esteeee : "+ response);
-    globalRides=response;
-    console.log("GLOBAL RIDES DUPA  GET NEW RIDES" +globalRides);
+    //   console.log("Raspunsul de la server esteeee : "+ response);
+    globalRides = response;
+    console.log("GLOBAL RIDES DUPA  GET NEW RIDES" + globalRides);
     //showRides()
 
     // const resultData = awa1it response.json();
@@ -159,18 +159,22 @@ async function getNewRides()
     }
     // username= JSON.stringify(response.body);
 }
-let username="";
+
+let username = "";
+
 async function changeUsername() {
     var x = document.getElementById("defaultTitle");
-    var user=document.getElementById("username")
-    if(x)
-    x.innerHTML =
-        `Bine ai revenit,${username}!`
+    var user = document.getElementById("username")
+    if (x)
+        x.innerHTML =
+            `Bine ai revenit,
+        ${username}!`
         ;
-    if(user)
-    user.innerHTML=`${username} <a href="#user" style="padding: 0;"> detalii profil</a>`;
+    if (user)
+        user.innerHTML = `${username} <a href="#user" style="padding: 0;"> detalii profil</a>`;
 
 }
+
 async function getUsername() {
     const data = {
         token: localStorage.getItem('token')

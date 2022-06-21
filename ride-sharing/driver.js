9e823b8154fa3a7befab0852dab66f60b69802e3
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-06-21 18:18:55 +0300

    fixed driver orders

--- ride-sharing/driver.js
+++ ride-sharing/driver.js
@@ -41,7 +41,30 @@ async function getService()
     }
     // username= JSON.stringify(response.body);
 }
+async function logout()
+{
 
+    const data={
+        token: localStorage.getItem('token')
+    }
+    console.log(data.token);
+    const response = await fetch('http://localhost:8000/api/logout', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        // mode: 'no-cors', // no-cors, *cors, same-origin
+        // headers: {
+        //     'Content-Type': 'application/json'
+        //     // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then(r => r.json())
+        .catch(e => {
+            console.log('error');
+            console.log(e);
+        });
+    localStorage.removeItem('token');
+
+    window.location.href="http://127.0.0.1:8000/mainHome/mainHome.html";
+}
 async function getClaimedRides() {
     const data = {
         token: localStorage.getItem('token')
@@ -189,7 +212,9 @@ async function getNewRides() {
             sent = false
         });
     //   console.log("Raspunsul de la server esteeee : "+ response);
+    if (globalRides)
     globalRides = response;
+    else globalRides=[];
     console.log("GLOBAL RIDES DUPA  GET NEW RIDES" + globalRides);
     //showRides()
 
